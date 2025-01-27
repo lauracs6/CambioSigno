593af0ff010571074241065d8e88a78e8ae78ca4
Laura Cabrera Sagrera <lauracabrerasagrera@gmail.com>
2025-01-27 11:09:43 +0100

    Update CambioSigno.java
    cambio 0 a 2

--- CambioSigno.java
+++ CambioSigno.java
@@ -1,7 +1,7 @@
 public class CambioSigno {
       
     public static int signChange(int [] arr) {
-        int counter = 0;
+        int counter = 2;
         
         for (int i = 0; i < arr.length - 1; i++) {
           if ((arr[i] >= 0 && arr[i + 1] < 0) || (arr[i] < 0 && arr[i + 1] >= 0)) {
